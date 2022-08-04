0009e144fdc6ee422a827f0f48318aab5ad0d5d0
elwina <elwina_vardal@outlook.com>
2022-08-04 20:31:09 +0800

    3 files modified

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,3 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
 export default async (req, res) => {
-  const db = await open({
-    filename: './database.db',
-    driver: sqlite3.Database,
-  });
-  await db.exec('CREATE TABLE tbl (col TEXT)');
   res.status(200).json({ name: 'sqlite' });
 };
